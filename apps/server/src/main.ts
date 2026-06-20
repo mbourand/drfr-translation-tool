8d1456cb018b6c96505426c8eebf7dc955214a5e
mbourand <shirowderable@gmail.com>
2026-06-20 21:44:59 +0200

    refactor(server): simplify import statements in main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
-import * as cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser'
 
-import * as dayjs from 'dayjs'
-import * as customParseFormat from 'dayjs/plugin/customParseFormat'
+import dayjs from 'dayjs'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
 import { json } from 'express'
 
 dayjs.extend(customParseFormat)
